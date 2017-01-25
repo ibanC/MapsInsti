f623f628fd6c4841e3b8d6ffbdee5a43e358587e
ibanC <iban@yopmail.com>
2017-01-25 09:53:13 +0100

    con animar

--- app/src/main/java/com/example/dm2/mapsinsti/MainActivity.java
+++ app/src/main/java/com/example/dm2/mapsinsti/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,12 +13,13 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mapa;
     private int vistaActual;
-    private Button animar;
+    private Button animar,posInicial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +34,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 animarInsti();
             }
         });
+
+        posInicial=(Button)findViewById(R.id.btnPosInicial);
+        posInicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LatLng insti=new LatLng(0,0);
+                CameraPosition camPos=new CameraPosition.Builder().target(insti).zoom(2).bearing(0).tilt(0).build();
+
+                CameraUpdate camUpd3= CameraUpdateFactory.newCameraPosition(camPos);
+                mapa.animateCamera(camUpd3);
+            }
+        });
     }
 
     private void animarInsti() {
-        LatLng insti=new LatLng(42.8370772,-2.6792914);
-        CameraPosition camPos=new CameraPosition.Builder().target(insti).zoom(17).bearing(45).build();
+        LatLng insti=new LatLng(42.837211449,-2.6774750792914);
+        CameraPosition camPos=new CameraPosition.Builder().target(insti).zoom(19).bearing(0).tilt(67.5f).build();
 
         CameraUpdate camUpd3= CameraUpdateFactory.newCameraPosition(camPos);
         mapa.animateCamera(camUpd3);
@@ -45,8 +59,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa=googleMap;
+        mapa.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Toast.makeText(MainActivity.this,"Lat"+cameraPosition.target.latitude+"lon:"+cameraPosition.target.longitude+" zoom"
+                +cameraPosition.zoom+"orientation"+cameraPosition.bearing+"angulo"+cameraPosition.tilt,Toast.LENGTH_LONG).show();
+            }
+        });
         vistaActual=GoogleMap.MAP_TYPE_SATELLITE;
         mapa.setMapType(vistaActual);
+        mapa.addMarker(new MarkerOptions().position(new LatLng(42.837211449,-2.6774750792914)).title("Ciudad Jardin"));
         mapa.getUiSettings().setZoomControlsEnabled(true);
     }
     public void cambiarVista(View v)
